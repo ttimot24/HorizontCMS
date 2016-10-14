569b97953096c2b8322bd2e55422f2a33911eaeb
Timot Tarjani <ttimotster@gmail.com>
2016-10-14 11:30:35 +0200

    Main menu translation: hu

---
+++ resources/lang/hu/navbar.php
@@ -0,0 +1,27 @@
+<?php
+return [
+	/*Main menus*/
+	'dashboard' => 'Vezérlőpult',
+	'news' => 'Blog',
+	'users' => 'Felhasználók',
+	'pages' => 'Oldalak',
+	'media' => 'Média',
+	'themes_apps' => 'Témák és Appok',
+  
+	/*Sumbmenus*/
+	'posted_news' => 'Minden poszt',
+	'create_post' => 'Új poszt',
+	'categories' => 'Kategóriák',
+	'user_list' => 'Felhasználó lista',
+	'user_add' => 'Új felhasználó',
+	'user_groups' => 'Felhasználói jogkörök',
+	'page_list' => 'Minden oldal',
+	'page_add' => 'Új oldal',
+	'header_images' => 'Fejléc képek',
+	'filemanager' => 'Fájlok',
+	'gallery' => 'Galéria',
+	'theme' => 'Témák',
+	'plugin' => 'Kiegészítők',
+	'develop' => 'Fejlesztés',
+	
+];
