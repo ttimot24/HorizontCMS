d3de8c424b56763791c480a1ab4955587ad9b355
Timot Tarjani <ttimotster@gmail.com>
2017-05-07 13:46:48 +0200

    Default theme name change on install

--- database/seeds/SettingsSeeder.php
+++ database/seeds/SettingsSeeder.php
@@ -47,7 +47,7 @@ class SettingsSeeder extends Seeder
 
         DB::table('settings')->insert([
             'setting' => 'theme',
-            'value' => 'thewright',
+            'value' => 'TheWright',
             'more' => '1',
         ]);
 
