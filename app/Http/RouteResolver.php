a8b4a4498b33111f04f41d02a5f29fbf3ee0d6bf
Timot Tarjani <ttimotster@gmail.com>
2017-08-28 14:22:02 +0200

    RouteResolver handles null and can reset default namespace

--- app/Http/RouteResolver.php
+++ app/Http/RouteResolver.php
@@ -4,23 +4,29 @@ namespace App\Http;
 
 class RouteResolver{
 
-	private $namespace = "\App\\Controllers\\";
+	private $defaultNamespace = "\App\\Controllers\\";
+
+	public function __construct(){
+		$this->resetNamespace();
+	}
+
+	public function resetNamespace(){
+		$this->namespace = $this->defaultNamespace;
+	}
 
 	public function changeNamespace($namespace){
 		$this->namespace = $namespace;
 	}
 
+	public function resolve($controller = 'dashboard',$action = null, $args = null){
 
-	public function resolve($controller = 'dashboard',$action = 'index', $args = null){
-
-				$action!=""? : $action='index';
+				$action!="" || $action===null ? : $action='index';
 		
-				$controller_path = lcfirst(ltrim(str_replace("\\",DIRECTORY_SEPARATOR,camel_case($this->namespace)),DIRECTORY_SEPARATOR));
 				$controller_name = studly_case($controller).'Controller';
 
 		        $controllerClass = $this->namespace.$controller_name;
 
-				//if(!file_exists($controller_path.$controller_name.'.php')){
+
 				if(!class_exists($controllerClass)){
 
 					throw new \App\Exceptions\FileNotFoundException('<b>'.$controllerClass.'.php'.'</b>');
