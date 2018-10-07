01f95fa4862b248433e716919d15d4b75131743d
Timot Tarjani <ttimotster@gmail.com>
2018-10-07 12:25:50 +0200

    CKEditor config update

--- resources/assets/ckeditor/config.js
+++ resources/assets/ckeditor/config.js
@@ -4,6 +4,7 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
+	config.skin = 'bootstrapck';
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
