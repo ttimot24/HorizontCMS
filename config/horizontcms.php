48dba03479d7b49aa933c4ae10abb42287550fc9
Timot Tarjani <ttimotster@gmail.com>
2019-01-12 12:56:22 +0100

    Use minified js versions

--- config/horizontcms.php
+++ config/horizontcms.php
@@ -39,8 +39,8 @@ return [
 	'js' => [
 			'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js',
 			'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
-			'resources/assets/filemaster/js/fileinput.js',
-			'https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.17/vue.js',
+			'resources/assets/filemaster/js/fileinput.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.17/vue.min.js',
 			'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js',
 			//'resources/assets/js/app.js',
 			//'resources/js/app.js' //compiled
