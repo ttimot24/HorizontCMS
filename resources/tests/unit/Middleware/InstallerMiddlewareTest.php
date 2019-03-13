2b7768847c20c1464d04cc8c90731f842ea95a31
Timot Tarjani <ttimotster@gmail.com>
2019-03-13 20:00:39 +0100

    InstallerMiddlewareTest added

---
+++ resources/tests/unit/Middleware/InstallerMiddlewareTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+
+class InstallerMiddlewareTest extends TestCase
+{
+
+    /** @test */
+    public function testNotRedirectIfInstalled()
+    {
+        $request = \Request::create('/admin/dashboard', 'GET');
+
+        $middleware = new \App\Http\Middleware\InstallerMiddleware;
+
+        $response = $middleware->handle($request, function () { });
+
+        $this->assertNull($response); 
+    }
+
+    /** @test */
+    public function testInstallerIsNotAvailableAfterInstalled()
+        {
+            $request = \Request::create('/admin/install', 'GET');
+    
+            $middleware = new \App\Http\Middleware\InstallerMiddleware;
+    
+            $response = $middleware->handle($request, function () { });
+    
+            $this->assertNotNull($response);
+            $this->assertEquals($response->getStatusCode(),302); 
+            $this->assertEquals($response->headers->get('Location'),\Config::get('app.url').'/admin/login');
+        }
+
+}
