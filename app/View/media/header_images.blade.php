4bc06d2b84d2536de909f17269fcdda7fceb69e0
Timot Tarjani <ttimotster@gmail.com>
2019-03-20 23:43:26 +0100

    Fix header image height

--- app/View/media/header_images.blade.php
+++ app/View/media/header_images.blade.php
@@ -25,8 +25,8 @@
 
 	@if(!$slider_images->isEmpty())
 		@foreach($slider_images as $image)
-			<div class='img-thumbnail col-md-3'>
-			<img src='storage/images/header_images/{{ $image->image }}' alt='' class='img-rounded' width='100%' height='85%;'>
+			<div class='img-thumbnail col-md-3' style='height:180px;'>
+			<img src='storage/images/header_images/{{ $image->image }}' alt='' class='img-rounded' width='100%' height='75%;'>
 			<p style="color:white;margin:0px;padding:0px;overflow:hidden;">{{$image->image}}</p>
 			<a class='btn btn-danger btn-xs btn-block' href='admin/header-image/delete/{{ $image->id }}'>Remove from slider</a>
 			</div>
@@ -43,13 +43,13 @@
 
 
 @foreach($dirs as $each)
-	<div class='col-md-3 img img-thumbnail' style='margin-bottom:5%;'>
+	<div class='col-md-3 img img-thumbnail' style='margin-bottom:3%;height:200px;'>
 	<a class='btn-sm btn-success col-md-6' href='admin/header-image/create/{{ $each }}'>Add to slider</a>
 	<a href='admin/file-manager/delete?file=storage/images/header_images/{{$each}}' class='pull-right'>
 	<span class='glyphicon glyphicon-remove' aria-hidden='true' style=' font-size: 1.4em;z-index:15;top:3px;right:3px;margin-bottom:-15px;'></span></a>
 
 	@if($each!="" && !is_dir('storage/images/header_images/{{ $each }}'))
-	<img src='storage/images/header_images/{{ $each }}' alt='' class='img-rounded' width='100%' height='80%;'>
+	<img src='storage/images/header_images/{{ $each }}' alt='' class='img-rounded' width='100%' height='75%;'>
 	@endif
 	<p style="color:white;margin:0px;padding:0px;overflow:hidden;">{{$each}}</p>
 	</div>
