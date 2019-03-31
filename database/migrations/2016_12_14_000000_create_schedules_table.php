f2b85c97f6b89089a8bd43f52530cd52b99e2723
Timot Tarjani <ttimotster@gmail.com>
2019-03-31 18:06:59 +0200

    Fix migration table class name

--- database/migrations/2016_12_14_000000_create_schedules_table.php
+++ database/migrations/2016_12_14_000000_create_schedules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePluginsTable extends Migration
+class CreateSchedulesTable extends Migration
 {
     /**
      * Run the migrations.
