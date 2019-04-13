22169cea054f6411ac1561523f57eaa7e110c7be
Timot Tarjani <ttimotster@gmail.com>
2019-04-13 16:17:54 +0200

    View log enhancements

--- app/View/settings/log.blade.php
+++ app/View/settings/log.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class='container'>
 <section class="row">
-<h1>System log <small class='pull-right' style='margin-top:1.5%;'>Files: {{$all_files->count()}} Entries: {{$all_file_entries}}</small></h1>
+<h1>System log <small class='pull-right' style='margin-top:1.5%;'>Files: {{$all_files->count()}}</small></h1>
 <br><br>
 
 <div class='col-md-4'>
@@ -34,7 +34,12 @@
 
 ?>
 
-
+<div class="well" style="padding:25px;">
+    <div class="row">
+      <h4 class="col-md-4" >Entries: {{$all_file_entries}}</h4>
+      <a href="{{$file}}" class="btn btn-primary pull-right"><i class="fa fa-download" aria-hidden="true"></i> Download file</a>
+    </div>
+</div>
 <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
   
 @foreach($entries as $entry)
@@ -44,7 +49,7 @@
       <h4 class="panel-title">
         <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse{{$entry_number}}" @if($loop->first) aria-expanded="true" @else aria-expanded="false" @endif aria-controls="collapse{{$entry_number}}">
              <div class='row'>
-              <div class='col-md-6'>#{{$entry_number}}  <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> {{ucfirst($entry->level)}} </div> <div class='col-md-6 text-right'>{{$entry->date}} </div>
+              <div class='col-md-8'>#{{$entry_number}}  <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> {{ucfirst($entry->level)}} - {{$entry->id}}</div> <div class='col-md-4 text-right'>{{$entry->date}} </div>
         	 </div>
         </a>
       </h4>
