b3fbd5e0a84ebcb17e18d88df4dd09f98f55b1dd
Timot Tarjani <ttimotster@gmail.com>
2019-05-04 14:34:39 +0200

    Show blogpost controller method test added

--- resources/tests/integration/BlogpostControllerTest.php
+++ resources/tests/integration/BlogpostControllerTest.php
@@ -10,7 +10,7 @@ class BlogpostControllerTest extends TestCase{
 
         \Event::fake();
 
-        $request = Request::create('/admin/blogpost/index', 'POST',[]);
+        $request = Request::create('/admin/blogpost/index', 'GET',[]);
 
         $controller = new \App\Controllers\BlogpostController($request, new \App\Libs\ViewResolver());
 
@@ -25,6 +25,33 @@ class BlogpostControllerTest extends TestCase{
         $this->assertTrue(isset($responseView->getData()['all_blogposts']));
         $this->assertInstanceOf(\App\Model\Blogpost::class,$responseView->getData()['all_blogposts'][0]);
     }
+
+
+    public function testShowAction(){
+
+        $idToTest = 1;
+
+        \Event::fake();
+
+        $request = Request::create('/admin/blogpost/show', 'POST',[]);
+
+        $controller = new \App\Controllers\BlogpostController($request, new \App\Libs\ViewResolver());
+
+        $responseView = $controller->show($idToTest);
+
+        $this->assertInstanceOf(\Illuminate\View\View::class,$responseView);
+
+        $this->assertEquals('blogposts.view', $responseView->name());
+
+
+        $this->assertTrue(isset($responseView->getData()['blogpost']));
+        $this->assertFalse(isset($responseView->getData()['previous_blogpost'])); //We checking the first blogpost
+        $this->assertTrue(isset($responseView->getData()['next_blogpost']));
+        $this->assertInstanceOf(\App\Model\Blogpost::class,$responseView->getData()['blogpost']);
+        
+        $this->assertEquals($idToTest,$responseView->getData()['blogpost']->id);
+    }
+
 
 
 }
