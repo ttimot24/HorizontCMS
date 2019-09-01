bc1c376357a917d10ea4baff3f56b48b88d66efd
Timot Tarjani <ttimotster@gmail.com>
2019-09-01 13:23:38 +0200

    ExceptionHandler render tests added

--- resources/tests/unit/ExceptionHandlerTest.php
+++ resources/tests/unit/ExceptionHandlerTest.php
@@ -11,6 +11,7 @@ class ExceptionHandler extends TestCase
     public function instantiateExceptionHandler(){
 
         $this->requestMock = \Mockery::mock(\Illuminate\Http\Request::class)->makePartial();
+        $this->requestMock->headers = new \Symfony\Component\HttpFoundation\HeaderBag([]);
 
         $this->exceptionHandler = new \App\Exceptions\Handler($this->createMock(\Illuminate\Contracts\Container\Container::class));
     }
@@ -21,5 +22,45 @@ class ExceptionHandler extends TestCase
         $this->assertInstanceOf(\Illuminate\Http\RedirectResponse::class,$response);
     }
 
-   
+    
+    public function testRenderAuthenticationException(){
+        
+        $response = $this->exceptionHandler->render($this->requestMock, new \Illuminate\Auth\AuthenticationException('Unauthenticated') );
+
+        $this->assertInstanceOf(\Illuminate\Http\RedirectResponse::class,$response);
+
+        $this->requestMock->headers->set('Accept', 'application/json');
+        $this->requestMock->headers->set('Content-Type', 'application/json');
+        $this->requestMock->headers->set('X-Requested-With', 'XMLHttpRequest');
+
+        $response = $this->exceptionHandler->render($this->requestMock, new \Illuminate\Auth\AuthenticationException('Unauthenticated') );
+
+        $this->assertInstanceOf(\Illuminate\Http\JsonResponse::class,$response);
+        
+    }
+
+    public function testRenderAllExceptions(){
+        $response = $this->exceptionHandler->render($this->requestMock, new \Exception('Any exception') );
+
+        $this->assertInstanceOf(\Illuminate\Http\Response::class,$response);
+        
+    }
+
+    /* TODO test dontReport */
+   /* public function testReport(){
+        $response = $this->exceptionHandler->report(new \Exception('Any exception'));
+    }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
