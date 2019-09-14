160f4ce0fd66433cd95777eb629b87b308eac09e
Timot Tarjani <ttimotster@gmail.com>
2019-09-14 20:45:51 +0200

    Default theme design modifications

--- themes/TheWright/page_templates/blog.php
+++ themes/TheWright/page_templates/blog.php
@@ -2,17 +2,18 @@
 
 	if(isset(Website::$_SLUGS[1])){
 		$blogpost = \App\Model\Blogpost::findBySlug(Website::$_SLUGS[1]);
-
+		echo "<div class='well'>";
 		echo "<h1>".$blogpost->title."</h1>";
 
 		if($blogpost->hasImage() && file_exists('storage/images/blogposts/'.$blogpost->image)){
-			echo "<img src='storage/images/blogposts/".$blogpost->image ."'>";
+			echo "<center><img src='storage/images/blogposts/".$blogpost->image ."'></center>";
 		}
 
 		echo "<h3>".$blogpost->summary."</h3>";
 
 		echo "<p>".$blogpost->text."</p>";
 
+		echo "</div>";
 		return;
 	}
 
@@ -24,12 +25,14 @@
 <?php $all_blogposts = \App\Model\Blogpost::orderBy('id','desc')->paginate(\Settings::get('blogposts_on_page')) ?>
 
 <?php foreach($all_blogposts as $blogpost): ?>
+<div class="well">
 	<?php if($blogpost->isDraft()){continue;} ?>
 	<img class="img-rounded" src="<?= $blogpost->getImage() ?>" style="width:100%;height:400px;object-fit:cover;">
 	<h2><a href="<?= str_slug(Website::$_REQUESTED_PAGE->name).'/'.str_slug($blogpost->title) ?>"><?= $blogpost->title ?></a></h2>
 	<p style="padding-left:5px;">Written by <a href="#"><?= $blogpost->author->username ?></a> on <a href="#"><?= $blogpost->created_at->diffForHumans() ?></a> in <a href="#"><?= $blogpost->category->name ?></a></p>
 	<p><b><?= $blogpost->getExcerpt() ?></b></p>
 	<p></p>
+</div>
 	<hr>
 <?php endforeach; ?>
 
