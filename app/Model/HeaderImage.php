7686cdb4ec4bad19980da3cd7b9ec8871cb6c1ce
Timot Tarjani <ttimotster@gmail.com>
2019-10-07 22:28:48 +0200

    HeaderImage get image with full path method added

--- app/Model/HeaderImage.php
+++ app/Model/HeaderImage.php
@@ -7,4 +7,14 @@ use \App\Libs\Model;
 class HeaderImage extends Model
 {
     public $timestamps = false;
+
+    public function getImage(){
+
+    	if($this->hasImage() && file_exists("storage/images/header_images/".$this->image)){
+    		return url("storage/images/header_images/".$this->image);
+    	}else{
+    		return "";
+    	}
+
+    }
 }
