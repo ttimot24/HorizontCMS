de4b8a442c73822b00422b939ff48aa722961c62
Timot Tarjani <ttimotster@gmail.com>
2019-12-25 12:02:48 +0100

    Settings panel list moved to a separate function

--- app/Controllers/SettingsController.php
+++ app/Controllers/SettingsController.php
@@ -36,7 +36,26 @@ class SettingsController extends Controller{
 
     }
 
- 
+    private function getSettingsPanels(){
+        
+        $backend_prefix = \Config::get('horizontcms.backend_prefix');
+
+        return [
+            ['name' => trans('settings.website'),'link' => $backend_prefix.'/settings/website','icon' => 'fa fa-globe'],
+            ['name' => trans('settings.admin_area'),'link' => $backend_prefix.'/settings/adminarea','icon' => 'fa fa-desktop'],
+            ['name' => trans('settings.update_center'),'link' => $backend_prefix.'/settings/updatecenter','icon' => 'fa fa-arrow-circle-o-up'],
+            ['name' => trans('settings.server'),'link' => $backend_prefix.'/settings/server','icon' => 'fa fa-server'],
+//                ['name' => trans('settings.email'),'link' => $backend_prefix.'/settings/email','icon' => 'fa fa-envelope'],
+            ['name' => trans('settings.social_media'),'link' => $backend_prefix.'/settings/socialmedia','icon' => 'fa fa-thumbs-o-up'],
+            ['name' => trans('Log'),'link' => $backend_prefix.'/settings/log', 'icon' => 'fa fa-bug'],
+            ['name' => trans('settings.database'),'link' => $backend_prefix.'/settings/database','icon' => 'fa fa-database'],
+            ['name' => trans('settings.scheduler'),'link' => $backend_prefix.'/settings/schedules','icon' => 'fa fa-clock-o'],
+//                  ['name' => trans('settings.spread'),'link' => $backend_prefix.'/settings/spread','icon' => 'fa fa-paper-plane'],
+//                    ['name' => trans('settings.uninstall'),'link' => $backend_prefix.'/settings/uninstall','icon' => 'fa fa-exclamation-triangle'],
+
+            ];
+
+    }
 
     /**
      * Display a listing of the resource.
@@ -45,27 +64,9 @@ class SettingsController extends Controller{
      */
     public function index($slug = 'index'){
 
-                $backend_prefix = \Config::get('horizontcms.backend_prefix');
-
-                $panels = [
-                    ['name' => trans('settings.website'),'link' => $backend_prefix.'/settings/website','icon' => 'fa fa-globe'],
-                    ['name' => trans('settings.admin_area'),'link' => $backend_prefix.'/settings/adminarea','icon' => 'fa fa-desktop'],
-                    ['name' => trans('settings.update_center'),'link' => $backend_prefix.'/settings/updatecenter','icon' => 'fa fa-arrow-circle-o-up'],
-                    ['name' => trans('settings.server'),'link' => $backend_prefix.'/settings/server','icon' => 'fa fa-server'],
-    //                ['name' => trans('settings.email'),'link' => $backend_prefix.'/settings/email','icon' => 'fa fa-envelope'],
-                    ['name' => trans('settings.social_media'),'link' => $backend_prefix.'/settings/socialmedia','icon' => 'fa fa-thumbs-o-up'],
-                    ['name' => trans('Log'),'link' => $backend_prefix.'/settings/log', 'icon' => 'fa fa-bug'],
-                    ['name' => trans('settings.database'),'link' => $backend_prefix.'/settings/database','icon' => 'fa fa-database'],
-                    ['name' => trans('settings.scheduler'),'link' => $backend_prefix.'/settings/schedules','icon' => 'fa fa-clock-o'],
-  //                  ['name' => trans('settings.spread'),'link' => $backend_prefix.'/settings/spread','icon' => 'fa fa-paper-plane'],
-//                    ['name' => trans('settings.uninstall'),'link' => $backend_prefix.'/settings/uninstall','icon' => 'fa fa-exclamation-triangle'],
-
-                    ];
-
-
         $this->view->title(trans('settings.settings'));
         return $this->view->render('settings/index',[
-                                                        'panels' => $panels,
+                                                        'panels' => $this->getSettingsPanels(),
                                                     ]);
     }
 
