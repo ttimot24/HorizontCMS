31ae9b8ac2ba907019bd42164d5acff32016f8b2
Timot Tarjani <ttimotster@gmail.com>
2022-02-19 15:30:00 +0100

    Added v1 prefix to REST Api

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -77,7 +77,7 @@ class RouteServiceProvider extends ServiceProvider
         Route::group([
             'middleware' => 'api',
             'namespace' => $this->namespace,
-            'prefix' => 'api',
+            'prefix' => 'api/v1',
         ], function ($router) {
             require base_path('routes/api.php');
         });
