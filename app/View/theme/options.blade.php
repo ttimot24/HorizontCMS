fb67b8cca717fb75fa7c6c1717d611b95f1758ea
Timot Tarjani <ttimotster@gmail.com>
2022-02-27 13:56:38 +0100

    Update options.blade.php

--- app/View/theme/options.blade.php
+++ app/View/theme/options.blade.php
@@ -4,26 +4,26 @@
 <div class='container'>
     <h2>Theme options</h2>
     
-    <div class="row pt-4">
-        <div class="col-2">
-            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-            <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Custom style</a>
-            <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Translations</a>
-        </div>
+
+    <div class="d-flex align-items-start pt-4">
+        <div class="nav flex-column nav-pills me-3 col-2" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <button class="nav-link active" id="v-pills-style-tab" data-bs-toggle="pill" data-bs-target="#v-pills-style" type="button" role="tab" aria-controls="v-pills-style" aria-selected="true">Custom style</button>
+            <button class="nav-link" id="v-pills-translate-tab" data-bs-toggle="pill" data-bs-target="#v-pills-translate" type="button" role="tab" aria-controls="v-pills-translate" aria-selected="false">Translations</button>
         </div>
         <div class="col-10">
-            <div class="tab-content" id="v-pills-tabContent">
-            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-            <h3 class="m-0 p-0">Custom Style</h3>
+        <div class="tab-content" id="v-pills-tabContent">
+            <div class="tab-pane fade show active" id="v-pills-style" role="tabpanel" aria-labelledby="v-pills-style-tab">
+                <h3 class="m-0 p-0">Custom Style</h3>
                 <div>
                     <form action="{{admin_link('settings-save')}}" method="POST">
                         {{csrf_field()}}
-                        <textarea rows="20" style="width:100%;" class='form-control' name="custom_css_{{snake_case($theme)}}" placeholder="Write your css here...">{{ isset($settings['custom_css_'.snake_case($theme)]) ? $settings['custom_css_'.snake_case($theme)] : ""  }}</textarea>
+                        <textarea rows="20" class='form-control' name="custom_css_{{snake_case($theme)}}" placeholder="Write your css here...">{{ isset($settings['custom_css_'.snake_case($theme)]) ? $settings['custom_css_'.snake_case($theme)] : ""  }}</textarea>
                         <input type="submit" class="btn btn-primary my-3" value="Save">
                     </form>
                 </div>
             </div>
-            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+            <div class="tab-pane fade" id="v-pills-translate" role="tabpanel" aria-labelledby="v-pills-translate-tab">
+            <h3 class="m-0 p-0">Translations</h3>
             <form action="{{admin_link('').'theme/update-translations/'.$theme}}" method="POST">
                 {{ csrf_field() }}
                 @foreach($translations as $lang => $value)
@@ -50,7 +50,7 @@
                 <input type="submit" class="btn btn-primary my-3" value="Save">
             </form>
             </div>
-            </div>
+        </div>
         </div>
     </div>
 
