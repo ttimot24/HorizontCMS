b793098685fba833b7dd1ecfcbef336190eadea4
Timot Tarjani <timot.tarjani@commsignia.com>
2022-09-27 08:49:41 +0200

    User edit mobile view fixes

--- app/View/users/edit.blade.php
+++ app/View/users/edit.blade.php
@@ -2,46 +2,43 @@
 
 @section('content')
 <div class='container main-container'>
-  <h2>{{trans('user.edit_user')}}</h2>
 
-<button type='button' class='btn btn-link pull-right' data-bs-toggle='modal' data-bs-target='#modal-xl-{{ $user->id }}'>
-  <img src='{{ $user->getThumb() }}' class='img img-thumbnail' width='320' >
-</button>
+  <h2>{{trans('user.edit_user')}}</h2>
 
 <form role='form' action="{{admin_link('user-update',$user->id)}}" method='POST' enctype='multipart/form-data'>
         {{ csrf_field() }}
 
     <div class="row">
- 
-      <div class='form-group pull-left col-xs-12 col-md-8' >
+      <div class="col-sm-12 col-md-8 ">
+      <div class='form-group' >
         <label for='title'>{{trans('user.create_name')}}:</label>
         <input type='hidden' class='form-control' id='title' name='id' value='{{ $user->id }}' >
         <input type='text' class='form-control' id='title' name='name' value='{{$user->name}}' required>
       </div>
 
-      <div class='form-group pull-left col-xs-12 col-md-8' >
+      <div class='form-group' >
         <label for='title'>{{trans('user.create_username')}}:</label>
         <input type='text' class='form-control' id='title' name='username' value='{{$user->username}}' required>
       </div>
 
         @if($user->is($current_user))
-          <div class='form-group pull-left col-xs-12 col-md-8' >
+          <div class='form-group' >
             <label for='title'>{{trans('user.create_password')}}:</label>
             <input type='password' class='form-control' id='title' name='password'  required>
           </div>
 
-          <div class='form-group pull-left col-xs-12 col-md-8' >
+          <div class='form-group' >
             <label for='title'>{{trans('user.create_password_again')}}:</label>
             <input type='password' class='form-control' id='title' name='password2'  required>
           </div>
         @endif
 
-      <div class='form-group pull-left col-xs-12 col-md-8' >
+      <div class='form-group' >
         <label for='title'>{{trans('user.create_email')}}:</label>
         <input type='email' class='form-control' id='title' name='email' value='{{$user->email}}' required>
       </div>
 
-      <div class='form-group pull-left col-xs-12 col-md-5' >
+      <div class='form-group' >
         <label for='sel1'>{{trans('user.create_select_rank')}}:</label>
         <select class='form-select' name='role_id' id='sel1'>
           
@@ -54,14 +51,23 @@
         </select>
       </div>
 
-      <div class='form-group pull-left col-xs-12 col-md-12' >
+    </div>
+
+    <div class="col-md-4 col-sm-12">
+      <button type='button' class='btn btn-link mb-5 w-100' data-bs-toggle='modal' data-bs-target='#modal-xl-{{ $user->id }}'>
+        <img src='{{ $user->getThumb() }}' class='img img-thumbnail' width='320' >
+      </button>
+
+      <div class='form-group' >
         <label for='file'>{{trans('actions.upload_image')}}:</label>
         <input name='up_file' accept='image/*' id='input-2' type='file' class='file' multiple='true' data-show-upload='false' data-show-caption='true'>
       </div>
 
-      <div class='form-group pull-left col-xs-12 col-md-12' > 
+    </div>
+
+    <div class='form-group' > 
         <button id='submit-btn' type='submit' class='btn btn-lg btn-primary'>{{trans('actions.update')}}</button>
-      </div>
+    </div>
      
     </div>
 
