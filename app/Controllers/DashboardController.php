b0c0ef070c6f2210b97334bc2ab9721efa35dcce
Timot Tarjani <timot.tarjani@commsignia.com>
2023-03-07 11:31:21 +0100

    Fix dashboard controller

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -14,23 +14,23 @@ class DashboardController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index()
     {
 
-        $admin_logo = $request->settings['admin_logo'];
+        $admin_logo = $this->request->settings['admin_logo'];
 
         $this->view->title(trans('dashboard.title'));
         return $this->view->render("dashboard/index", [
 
-            'domain' => $request->getHost(),
+            'domain' => $this->request->getHost(),
             'server_ip' => isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : "unknown",
-            'client_ip' => $request->ip(),
+            'client_ip' => $this->request->ip(),
             'blogposts'  => \App\Model\Blogpost::count(),
             'users' => \App\Model\User::count(),
             'visits' => \App\Model\Visits::count(),
             'admin_logo' => ($admin_logo != "" && file_exists("storage/images/logos/" . $admin_logo)) ? "storage/images/logos/" . $admin_logo : \Config::get('horizontcms.admin_logo'),
             'disk_space' => @(disk_free_space("/") / disk_total_space("/")) * 100,
-            'upgrade' => $request->settings['auto_upgrade_check'] == 1 && \Auth::user()->hasPermission('settings') ? \App\Model\SystemUpgrade::checkUpgrade() : NULL,
+            'upgrade' => $this->request->settings['auto_upgrade_check'] == 1 && \Auth::user()->hasPermission('settings') ? \App\Model\SystemUpgrade::checkUpgrade() : NULL,
 
         ]);
     }
