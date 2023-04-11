7f010355795998aa2390c030e44b4d7373713b8d
Timot Tarjani <timot.tarjani@commsignia.com>
2023-04-11 13:47:14 +0200

    Login page layout improvements

--- app/View/auth/login.blade.php
+++ app/View/auth/login.blade.php
@@ -1,52 +1,65 @@
 @extends('layout')
-    <div class='jumbotron'>
-        <div class='container'>
-       
-        <img src='{{$admin_logo}}' width='100' class='pull-left' alt="">
-
-            <h1 class="pt-3">{{$app_name}}</h1>
-            <p style='margin-left:15%;'><q><i>Closer to the web </i></q></p>
+<div class='jumbotron'>
+    <div class='container'>
+        <div class="row">
+            <div class="col-md-2 col-xs-12 text-center pt-3">
+                <img src='{{ $admin_logo }}' width="150" alt="">
+            </div>
+            <div class="col-md-4 col-xs-12 text-center">
+                <h1 class="pt-3">{{ $app_name }}</h1>
+                <p><q><i>Closer to the web</i></q></p>
+            </div>
+            <div class="col-md-6">
+            </div>
         </div>
     </div>
+</div>
 
-    <div class='container'>
-        <form action="{{ route('login') }}" role='form' method='POST' >
-            @csrf
-            <div class="row">
-                <div class='col-xs-12 col-md-3 pull-left'>  
+<div class='container'>
+    <form action="{{ route('login') }}" role='form' method='POST'>
+        @csrf
+        <div class="row">
+            <div class="container">
+                <div class='col-xs-12 col-md-4 p-5'>
                     <div class="form-group">
-                        <label for='text'>{{trans('login.username')}}:</label>
-                        <input type='text' class="form-control  @error('username') is-invalid @enderror" id='username' name='username' placeholder="{{trans('login.enter_username')}}" autofocus required>
-                            @error('username')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('username') }}</strong>
-                                </span>
-                            @enderror
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for='pwd'>{{trans('login.password')}}:</label>
-                        <input type='password' class="form-control  @error('password') is-invalid @enderror" id='pwd' name='password' placeholder="{{trans('login.enter_password')}}" required>
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
-                            @enderror
+                        <label for='text'>{{ trans('login.username') }}:</label>
+                        <input type='text' class="form-control  @error('username') is-invalid @enderror"
+                            id='username' name='username' placeholder="{{ trans('login.enter_username') }}" autofocus
+                            required>
+                        @error('username')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('username') }}</strong>
+                            </span>
+                        @enderror
                     </div>
 
-                    @if ($errors->has('username') || $errors->has('password'))
-                        <div>
-                            <b><a href="admin/password/reset" style="font-size:10px;">{{trans('login.forgot_password')}}</a></b>
+                    <div class="form-group">
+                        <label for='pwd'>{{ trans('login.password') }}:</label>
+                        <input type='password' class="form-control  @error('password') is-invalid @enderror"
+                            id='pwd' name='password' placeholder="{{ trans('login.enter_password') }}" required>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="container">
+                        <div class="row">
+
+                            <input type='submit' name='submit_login' class='btn btn-default col-6'
+                                value='{{ trans('login.login') }}'>
+
+                            @if ($errors->has('username') || $errors->has('password'))
+                                <div class="col-6 text-center">
+                                    <b><a href="{{ route('password.reset') }}"
+                                            style="font-size:10px;">{{ trans('login.forgot_password') }}</a></b>
+                                </div>
+                            @endif
                         </div>
-                    @endif
-                    
-                  
-                    <div class="form-check pl-0 pb-2">
-                        <input type="checkbox" class="remember_pass" id="remember_pass">
-                        <label class="form-check-label" for="remember_pass"> {{trans('login.remember_me')}}</label>
                     </div>
-                    <input type='submit' name='submit_login' class='btn btn-default' value='{{trans("login.login")}}'>
                 </div>
             </div>
-        </form>
-    </div>
+        </div>
+    </form>
+</div>
--- app/View/auth/passwords/email.blade.php
+++ app/View/auth/passwords/email.blade.php
@@ -1,56 +1,64 @@
 @extends('layout')
 
-    <div class='jumbotron'>
-        <div class='container'>
-       
-        <img src='{{$admin_logo}}' width='100' class='pull-left' alt="">
-
-            <h1>{{$app_name}}</h1>
-            <p style='margin-left:15%;'><q><i>Closer to the web </i></q></p>
-        </div>
-    </div>
-
-<!-- Main Content -->
-@section('content')
-<div class="container">
-    <br>
-    <div class="row">
-        <div class="col-md-8 offset-md-2">
-            <div class="panel panel-default" style="background:transparent;">
-                <div class="panel-heading text-white p-2" style="background-color:#1f1f1f;">Reset Password</div>
-                <div class="panel-body pt-3" style="color:black;">
-                    @if (session('status'))
-                        <div class="alert alert-success alert-dismissible">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-12">E-Mail Address</label>
-
-                            <div class="col-md-12">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group text-center">
-                            <button type="submit" class="btn btn-primary">
-                                Send Password Reset Link
-                            </button>
-                        </div>
-                    </form>
-                </div>
+<div class='jumbotron'>
+    <div class='container'>
+        <div class="row">
+            <div class="col-md-2 col-xs-12 text-center pt-3">
+                <img src='{{ $admin_logo }}' width="150" alt="">
+            </div>
+            <div class="col-md-4 col-xs-12 text-center">
+                <h1 class="pt-3">{{ $app_name }}</h1>
+                <p><q><i>Closer to the web</i></q></p>
+            </div>
+            <div class="col-md-6">
             </div>
         </div>
     </div>
 </div>
+
+
+<!-- Main Content -->
+@section('content')
+    <div class="container">
+        <br>
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="panel panel-default" style="background:transparent;">
+                    <div class="panel-heading text-white p-2" style="background-color:#1f1f1f;">Reset Password</div>
+                    <div class="panel-body pt-3" style="color:black;">
+                        @if (session('status'))
+                            <div class="alert alert-success alert-dismissible">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
+                            @csrf
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-12">E-Mail Address</label>
+
+                                <div class="col-md-12">
+                                    <input id="email" type="email" class="form-control" name="email"
+                                        value="{{ old('email') }}" required>
+
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group text-center">
+                                <button type="submit" class="btn btn-primary">
+                                    Send Password Reset Link
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
