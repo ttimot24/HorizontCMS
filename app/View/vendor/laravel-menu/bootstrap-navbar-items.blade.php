345044b96d158b92650de614c7acafedae24eeb9
Timot Tarjani <timot.tarjani@commsignia.com>
2023-05-20 16:30:40 +0200

    Fix lavary/laravel-menu template

--- app/View/vendor/laravel-menu/bootstrap-navbar-items.blade.php
+++ app/View/vendor/laravel-menu/bootstrap-navbar-items.blade.php
@@ -1,28 +1,30 @@
 @foreach ($items as $item)
-    <li @lm_attrs($item) @if ($item->hasChildren()) class="nav-item dropdown" @endif @lm_endattrs>
+    <li class="nav-item  {{ $item->hasChildren() ? 'dropdown"' : '' }}">
         @if ($item->link)
-            <a @lm_attrs($item->link) @if ($item->hasChildren())
-                class="nav-link dropdown-toggle" role="button" @data_toggle_attribute="dropdown" aria-haspopup="true"
+            <a @if ($item->hasChildren()) class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-haspopup="true"
                 aria-expanded="false"
             @else
-                class="nav-link"
-        @endif @lm_endattrs href="{!! $item->url() !!}">
-        {!! $item->title !!}
-        @if ($item->hasChildren())
-            <b class="caret"></b>
+                class="nav-link" @endif
+                href="{!! $item->url() !!}"> {!! $item->title !!}
+
+                @if ($item->hasChildren())
+                    <b class="caret"></b>
+                @endif
+
+            </a>
+        @else
+            <span class="navbar-text">{!! $item->title !!}</span>
         @endif
-        </a>
-    @else
-        <span class="navbar-text">{!! $item->title !!}</span>
-@endif
-@if ($item->hasChildren())
-    <ul class="dropdown-menu bg-dark m-0">
-        @include(config('laravel-menu.views.bootstrap-items'), ['items' => $item->children()])
-    </ul>
-@endif
-</li>
-@if ($item->divider)
-    <li{!! Lavary\Menu\Builder::attributes($item->divider) !!}>
-        </li>
-@endif
+
+        @if ($item->hasChildren())
+            <ul class="dropdown-menu bg-dark m-0">
+                @include(config('laravel-menu.views.bootstrap-items'), ['items' => $item->children()])
+            </ul>
+        @endif
+
+    </li>
+
+    @if ($item->divider)
+        <li {!! Lavary\Menu\Builder::attributes($item->divider) !!}></li>
+    @endif
 @endforeach
