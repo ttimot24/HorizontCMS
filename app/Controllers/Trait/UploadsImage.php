755c37d8b2f9cb58bd782c09834d46dc0a4acb78
Timot Tarjani <timot.tarjani@commsignia.com>
2024-04-12 17:02:55 +0200

    Fix type check in UploadsImage trait

--- app/Controllers/Trait/UploadsImage.php
+++ app/Controllers/Trait/UploadsImage.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Trait;
 
 use TypeError;
+use Illuminate\Support\Facades\File;
 
 trait UploadsImage {
 
@@ -14,18 +15,17 @@ trait UploadsImage {
 
     public function uploadImage($model): bool {
 
-        if(in_array('HasImage', class_uses($model, true))){
+        if(!in_array('App\Model\Trait\HasImage', class_uses($model, true))){
            throw new TypeError('Class['.get_class($model).'] does not use HasImage trait');
         }
 
         if (request()->hasFile($this->form_field_name)) {
 
-            \File::ensureDirectoryExists($this->getStrippedDirectoryPath($model));
+            File::ensureDirectoryExists($this->getStrippedDirectoryPath($model));
             $img = request()->up_file->store($this->getStrippedDirectoryPath($model));
             $model->attachImage($img);
             if (extension_loaded('gd')) {
-
-		        \File::ensureDirectoryExists($this->getStrippedDirectoryPath($model) . '/thumbs');
+		        File::ensureDirectoryExists($this->getStrippedDirectoryPath($model) . '/thumbs');
                 \Intervention\Image\ImageManagerStatic::make(storage_path($img))->fit(300, 200)->save($model->getThumbnailDirectory(). DIRECTORY_SEPARATOR . $model->image);
             }
 
