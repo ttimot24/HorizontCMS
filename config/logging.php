102f83197cdf3b6ddbfa7425c7385e7ce9094e61
Timot Tarjani <timot.tarjani@commsignia.com>
2024-05-30 15:20:33 +0200

    Use stack channel for logging by default

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'daily'),
+    'default' => env('LOG_CHANNEL', 'stack'),
 
     /*
     |--------------------------------------------------------------------------
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily', 'stderr'],
             'ignore_exceptions' => false,
         ],
 
