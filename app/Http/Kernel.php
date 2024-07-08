fbfb654e2ca14376c3ab332647de85e0242638c8
Timot Tarjani <ttimotster@gmail.com>
2024-07-08 13:30:04 +0000

    Cors handler middleware added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
     ];
 
