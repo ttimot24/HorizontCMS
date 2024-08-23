7284b6be20b249a89c51539ffb29b4a17f2f2a01
Timot Tarjani <ttimotster@gmail.com>
2024-08-23 13:56:46 +0200

    Fix ContentSeeder

--- database/seeders/ContentSeeder.php
+++ database/seeders/ContentSeeder.php
@@ -77,6 +77,7 @@ class ContentSeeder extends Seeder
             'id' => 1,
             'title' => 'default',
             'image' => 'abovethecity.jpg',
+            'author_id' => 1,
             'active' => 1
         ]);
 
