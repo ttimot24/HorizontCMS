51346c9b5ce440b0f252a87d4257b66ebd96fceb
Timot Tarjani <ttimotster@gmail.com>
2025-03-13 23:02:27 +0100

    Load Theme CLI commands

--- app/Providers/ThemeServiceProvider.php
+++ app/Providers/ThemeServiceProvider.php
@@ -19,10 +19,11 @@ class ThemeServiceProvider extends ServiceProvider
 
             $theme = new Theme(\App\Model\Settings::get('theme'));
 
-            $this->app->singleton(Theme::class, function ($app) use($theme) {
+            $this->app->singleton(Theme::class, function ($app) use ($theme) {
                 return $theme;
             });
 
+            $this->registerThemeAutoLoaders($theme);
 
             $this->registerTranslations($theme);
 
@@ -30,16 +31,31 @@ class ThemeServiceProvider extends ServiceProvider
 
             $this->registerThemeRoutes($theme);
 
+            $this->registerThemeConsoleCommands($theme);
         }
     }
 
-    protected function registerTranslations(Theme $theme){
+    private function registerThemeAutoLoaders(Theme $theme)
+    {
+
+
+
+
+        $autoloader = $theme->getPath() . "/vendor/autoload.php";
+        if (file_exists($autoloader)) {
+            require_once($autoloader);
+        }
+    }
+
+    protected function registerTranslations(Theme $theme)
+    {
         if (!\Request::is(\Config::get('horizontcms.backend_prefix') . "/*")) {
             $this->loadJsonTranslationsFrom(base_path($theme->getPath() . "resources/lang"));
         }
     }
 
-    protected function registerThemeViews(Theme $theme){
+    protected function registerThemeViews(Theme $theme)
+    {
         \View::addNamespace('theme', [
             $theme->getPath() . "app" . DIRECTORY_SEPARATOR . "View",
             $theme->getPath() . "resources" . DIRECTORY_SEPARATOR . "views",
@@ -70,12 +86,33 @@ class ThemeServiceProvider extends ServiceProvider
         }
     }
 
+    private function registerThemeConsoleCommands(Theme $theme)
+    {
+
+        try {
+            $_THEME_COMMANDS_PATH = $theme->getPath() . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Console';
+
+            foreach (array_diff(scandir($_THEME_COMMANDS_PATH), ['.', '..']) as $file) {
+                if (is_file($_THEME_COMMANDS_PATH . "/" . $file)) {
+
+                    require_once($_THEME_COMMANDS_PATH . "/" . $file);
+                    $actualName = pathinfo($file, PATHINFO_FILENAME);
+
+                    if (class_exists("\Theme\\" . $theme->getName() . "\App\Console\\" . $actualName, true)) {
+                        $this->commands(["\Theme\\" . $theme->getName() . "\App\Console\\" . $actualName]);
+                    }
+                }
+            }
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            throw $e;
+        }
+    }
+
     /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
-    {
-    }
+    public function register() {}
 }
